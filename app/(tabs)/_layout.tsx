2ad2025c4e15e6a6d7a90dc447cc294dae222fd0
matheusmanoelberto <matheusmanoel51@gmail.com>
2024-11-02 19:27:27 -0300

    home

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#0000FFS" }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Início",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+{
+  /* <Tabs.Screen
+        name="categories"
+        options={{
+          title: "Categorias",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="archive" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "Sobre mim",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
+        }}
+      /> */
+}
