425a0579be24014242f0ef66d936953bd7b10b3d
matheusmanoelbert <matheusmanoel51@gmail.com>
2024-12-03 20:02:27 -0300

    productId

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,19 +1,41 @@
-import { Stack } from "expo-router";
+import { router, Stack, useLocalSearchParams } from "expo-router";
 import {
   Button,
+  Image,
   SafeAreaView,
   ScrollView,
   StyleSheet,
+  Text,
   View,
 } from "react-native";
+import { getProductById } from "../../services/products";
 
 export default function Scree() {
-  const handleBuyButton = () => {};
+  const { id } = useLocalSearchParams();
+  const idProduct = parseInt(id as string);
+
+  const product = getProductById(idProduct);
+  if (!product) return router.back();
+
+  const handleBuyButton = () => {
+    alert("Você clicou no item: " + id);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{ title: "" }} />
-      <ScrollView style={styles.area}></ScrollView>
+      <ScrollView style={styles.area}>
+        <Image
+          style={styles.img}
+          source={{ uri: product.image }}
+          resizeMode="cover"
+        />
+        <Text style={styles.title}>{product.title}</Text>
+        <Text style={styles.description}>{product.description}</Text>
+        <View style={styles.priceArea}>
+          <Text style={styles.price}>R$ {product.price.toFixed(2)}</Text>
+        </View>
+      </ScrollView>
       <View style={styles.buttonArea}>
         <Button title="Compar Agora" onPress={handleBuyButton} />
       </View>
@@ -31,5 +53,30 @@ const styles = StyleSheet.create({
   },
   buttonArea: {
     padding: 10,
+  },
+  img: {
+    width: "100%",
+    height: 250,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 27,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 15,
+    color: "#555555",
+    marginBottom: 20,
+  },
+  priceArea: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#CCCCCC",
+  },
+  price: {
+    fontSize: 22,
+    textAlign: "center",
   },
 });
